fced93ccbb97531cc95312b6f3b3287c4b0acee2
akbarwibowo <akbarwibowo0123@gmail.com>
2024-06-18 17:10:53 +0700

    add vercel dependencies

--- cbr_cf_model/server.py
+++ cbr_cf_model/server.py
@@ -11,7 +11,3 @@ def home():
 def main():
     data = request.get_json()
     return jsonify(run_prediction(data))
-
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80)
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from cbr_cf_model.server import app
+
+if __name__ == "__main__":
+    app.run()
